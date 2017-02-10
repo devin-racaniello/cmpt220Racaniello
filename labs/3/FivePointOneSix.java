26466ee7f3f28a70e0fcc9035b741f40e0e442e8
Juan Arias <juan.arias@marist.edu>
2017-02-09 19:55:15 -0500

    Feedback on Lab 3

--- labs/3/FivePointOneSix.java
+++ labs/3/FivePointOneSix.java
@@ -10,6 +10,10 @@ public class FivePointOneSix {
 		System.out.print("Enter a number and its factors will be listed from smallest to greatest: ");
 		int num = input.nextInt();
 		
+		// JA: When finding the smallest factors, you need to decompose the numbers
+		// JA: For example, if the input is 340, you need to find the smallest number that is a factor 
+		// JA: and divide by the number, i.e., 2 is a factor and now your number is 170. You keep doing that 
+		// JA: until the number is 1. So the factors of 340 are 2,2,5,17.
 		for (int i = 1; i < (num+1); i++){
 			if (num%i == 0) {
 				System.out.print(i + ", ");
